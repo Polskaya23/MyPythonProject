324546452c01d4bf540d40f057951ad2539c838a
Tetiana Polska <cutyonly23@gmail.com>
2025-06-29 22:50:13 -0400

    Lesson 5.2 homework

---
+++ lesson 5.2.py
@@ -0,0 +1,27 @@
+while True:
+    num1 =float(input())
+    operation = input()
+    num2 = float(input())
+    if operation == '+':
+        result = num1 + num2
+        print(result)
+    elif operation == '-':
+        result = num1 - num2
+        print(result)
+    elif operation == '*':
+        result = num1 * num2
+        print(result)
+    elif operation == '/':
+        if num2 ==0:
+            print("Error: division by zero is not possible")
+        else:
+            result = num1 / num2
+            print(result)
+    else:
+        print("Error: invalid operation")
+
+    answer = input("Do you want to continue? (y/n) ")
+    if answer == 'y':
+        continue
+    else:
+        break

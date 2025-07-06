a08a27444182404d427214005f5e460a789140fc
Tetiana Polska <cutyonly23@gmail.com>
2025-07-06 18:50:20 -0400

    Lesson 7.2 homework

---
+++ lesson 7.2.py
@@ -0,0 +1,13 @@
+def correct_sentence(text: str):
+    if not text.endswith('.'):
+        return text[0].upper() + text[1:] + '.'
+    else:
+        return text[0].upper() + text[1:]
+
+
+assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
+assert correct_sentence("hello") == "Hello.", 'Test2'
+assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
+assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
+assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
+print('ОК')
